8fb4a5e2d99062095c556415e07a6367f2f287c2
ShubhamShinde06 <officialshubhamorg@gmail.com>
2025-08-15 19:09:04 +0530

    Not found page add

---
+++ Frontend/app/not-found.js
@@ -0,0 +1,6 @@
+// app/not-found.js
+import NotFoundContent from "@/components/NotFoundContent";
+
+export default function NotFoundPage() {
+  return <NotFoundContent />;
+}
---
+++ Frontend/components/NotFoundContent.jsx
@@ -0,0 +1,9 @@
+// components/NotFoundContent.js
+export default function NotFoundContent() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="mt-2 text-gray-500">Oops! Page not found.</p>
+    </div>
+  );
+}
