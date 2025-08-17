2aec2265b47b3c838ef2f82a563409da730da885
ShubhamShinde06 <officialshubhamorg@gmail.com>
2025-08-17 19:34:36 +0530

    animation addon

--- Frontend/provider/AppLayout.jsx
+++ Frontend/provider/AppLayout.jsx
@@ -84,7 +84,7 @@ function AppLayoutContent({ children }) {
 export default function AppLayout(props) {
   return (
     <Suspense fallback={
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center w-full h-full">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-white"></div>
       </div>
     }>
